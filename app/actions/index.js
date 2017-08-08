5c8b9d122d6e2aeae0feeaf2d03076bf50989364
JaredTan <jared.e.tan@gmail.com>
2017-08-08 04:44:13 -0700

    added auth, and to-do basic CRUD app

---
+++ app/actions/index.js
@@ -0,0 +1,3 @@
+export * from './auth_actions';
+export * from './alert_actions';
+export * from './todo_actions';
---
+++ app/components/app.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar
+} from 'react-native';
+
+// import {} from '../actions';
+
+import Login from './login';
+import Main from './main';
+import AlertContainer from './alerts/alert_container';
+
+var App = React.createClass({
+  getInitialState() {
+    return {}
+  },
+  render() {
+    var renderMainView = () => {
+      if (this.props.user_id) {
+        return (
+          <Main />
+        );
+      } else {
+        return (
+          <Login />
+        );
+      }
+    }
+    return (
+      <View style={{flex: 1}}>
+
+        {renderMainView()}
+        <AlertContainer/>
+      </View>
+    )
+  }
+});
+      // <StatusBar barStyle="light-content"/>
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 20,
+    backgroundColor: '#ccc'
+  },
+});
+
+var mapStateToProps = (state) => {
+  return {
+    user_id: state.auth.user_id
+  }
+}
+
+module.exports = connect(mapStateToProps)(App);
