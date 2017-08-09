1317c29dee0dbef063b615264cc984a88d9853df
Keith Ryan Wong <KeithRyanWong@gmail.com>
2017-08-09 00:53:51 -0700

    Separate animation into component

--- app/components/sprite.js
+++ app/components/sprite.js
@@ -8,33 +8,8 @@ import {
   NavigatorIOS
 } from 'react-native';
 
-const IMAGES = {
-  image0: require('../assets/spritesheets/braid/braid_frame00.png'),
-  image1: require('../assets/spritesheets/braid/braid_frame01.png'),
-  image2: require('../assets/spritesheets/braid/braid_frame02.png'),
-  image3: require('../assets/spritesheets/braid/braid_frame03.png'),
-  image4: require('../assets/spritesheets/braid/braid_frame04.png'),
-  image5: require('../assets/spritesheets/braid/braid_frame05.png'),
-  image6: require('../assets/spritesheets/braid/braid_frame06.png'),
-  image7: require('../assets/spritesheets/braid/braid_frame07.png'),
-  image8: require('../assets/spritesheets/braid/braid_frame08.png'),
-  image9: require('../assets/spritesheets/braid/braid_frame09.png'),
-  image10: require('../assets/spritesheets/braid/braid_frame10.png'),
-  image11: require('../assets/spritesheets/braid/braid_frame11.png'),
-  image12: require('../assets/spritesheets/braid/braid_frame12.png'),
-  image13: require('../assets/spritesheets/braid/braid_frame13.png'),
-  image14: require('../assets/spritesheets/braid/braid_frame14.png'),
-  image15: require('../assets/spritesheets/braid/braid_frame15.png'),
-  image16: require('../assets/spritesheets/braid/braid_frame16.png'),
-  image17: require('../assets/spritesheets/braid/braid_frame17.png'),
-  image18: require('../assets/spritesheets/braid/braid_frame18.png'),
-  image19: require('../assets/spritesheets/braid/braid_frame19.png'),
-  image20: require('../assets/spritesheets/braid/braid_frame20.png'),
-  image21: require('../assets/spritesheets/braid/braid_frame21.png'),
-  image22: require('../assets/spritesheets/braid/braid_frame22.png'),
-  image23: require('../assets/spritesheets/braid/braid_frame23.png'),
-};
-
+import NavBar from './navbar';
+import IMAGES from '../assets/spritesheets/sprites';
 
 
 class Sprite extends React.Component {
@@ -58,13 +33,14 @@ class Sprite extends React.Component {
       this.setState({
         frame: (this.state.frame + 1) % 24
       });
-    }, 200);
+    }, 100);
     return (
       <View style={styles.container}>
         <Image 
           source={this.getImage(this.state.frame)}  
           style={styles.sprite}
           />
+        <NavBar navigator={this.props.navigator}/>
       </View>
     );
   }
@@ -73,13 +49,14 @@ class Sprite extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingTop: 20,
-    backgroundColor: '#ccc'
+    justifyContent: 'flex-start',
+    alignItems: 'stretch',
   },
   sprite: {
-    resizeMode: 'stretch'
+    flex: 1,
+    alignSelf: 'center',
+    justifyContent: 'center',
+    resizeMode: 'center'
   }
 });
 
