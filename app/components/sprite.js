ef508736ddd96c95a5e96dd47dd697d3fda9482d
Janet Lee <lee.cjanet@gmail.com>
2017-08-09 23:04:42 -0700

    merging sprite to master

--- app/components/sprite.js
+++ app/components/sprite.js
@@ -8,37 +8,53 @@ import {
   NavigatorIOS
 } from 'react-native';
 
-import NavBar from './navbar';
+
 import IMAGES from '../assets/spritesheets/sprites';
+import BACKGROUND from '../assets/spritesheets/background/background';
 
 
 class Sprite extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      frame: 0,
-      tick: 0,
+      plantframe: 0,
+      bgframe: 0,
     };
 
     this.getImage = this.getImage.bind(this);
   }
 
-  getImage(num) {
-    return IMAGES['image' + num];
+  getImage(arr, num) {
+    return arr['image' + num];
   }
-  
+
   render() {
     setTimeout(() => {
       this.setState({
-        frame: (this.state.frame + 1) % 24
+        plantframe: (this.state.plantframe + 1) % 24
       });
     }, 200);
+
+    setTimeout(() => {
+       this.setState({
+         bgframe: (this.state.bgframe + 1) % 1
+       });
+     }, 3600); //one hour
+
     return (
       <View style={styles.container}>
-        <Image
-          source={this.getImage(this.state.frame)}
-          style={styles.sprite}
-          />
+        <View style={styles.backgroundlayer}>
+          <Image
+            source={this.getImage(BACKGROUND, this.state.bgframe)}
+            style={styles.background}
+            />
+        </View>
+        <View style={styles.overlay}>
+          <Image
+            source={this.getImage(IMAGES, this.state.plantframe)}
+            style={styles.plant}
+            />
+        </View>
       </View>
     );
   }
@@ -50,12 +66,28 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
     alignItems: 'stretch',
   },
-  sprite: {
-    flex: 1,
-    alignSelf: 'center',
-    justifyContent: 'center',
-    resizeMode: 'center'
-  }
+  backgroundlayer: {
+    position: 'absolute',
+     alignSelf: 'center',
+     justifyContent: 'center',
+     resizeMode: 'cover',
+    },
+    background: {
+     alignSelf: 'center',
+     justifyContent: 'center',
+     resizeMode: 'cover',
+   },
+   plant: {
+    //  bottom: '15',
+    //  alignSelf: 'center',
+    //  justifyContent: 'center',
+     backgroundColor: 'transparent',
+   },
+   overlay: {
+     alignSelf: 'center',
+     justifyContent: 'center',
+     backgroundColor: 'transparent',
+    }
 });
 
 export default Sprite;
