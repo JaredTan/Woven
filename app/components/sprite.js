c68e2fc2d5148ba4abd92700d0695a0884078cd2
Keith Ryan Wong <KeithRyanWong@gmail.com>
2017-08-09 19:13:05 -0700

    Use react animated to animate text

--- app/components/sprite.js
+++ app/components/sprite.js
@@ -5,40 +5,67 @@ import {
   View,
   Image,
   TouchableOpacity,
-  NavigatorIOS
+  NavigatorIOS,
+  Animated
 } from 'react-native';
 
 import NavBar from './navbar';
 import IMAGES from '../assets/spritesheets/sprites';
 
-
-class Sprite extends React.Component {
+class Animation extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      frame: 0,
-      tick: 0,
+      sprite: new Animated.Value(0),  // Initial value for opacity: 0
     };
-
-    this.getImage = this.getImage.bind(this);
+    this.increaseSize = this.increaseSize.bind(this);
+    this.resetSize = this.resetSize.bind(this);
   }
 
-  getImage(num) {
-    return IMAGES['image' + num];
+  componentDidMount() {
+    this.increaseSize();                     // Starts the animation
   }
-  
+
+  resetSize() {
+    this.setState({
+      sprite: new Animated.Value(0)
+    });
+    this.increaseSize();
+  }
+
+  increaseSize() {
+    Animated.timing(                  // Animate over time
+      this.state.sprite,            // The animated value to drive
+      {
+        toValue: 100,                   // Animate to opacity: 1 (opaque)
+        duration: 1000,              // Make it take a while
+      }
+    ).start(this.resetSize);  
+  }
+
   render() {
-    setTimeout(() => {
-      this.setState({
-        frame: (this.state.frame + 1) % 24
-      });
-    }, 200);
+    let { sprite } = this.state;
+
+    return (
+      <Animated.Text                 // Special animatable View
+        style={{
+          fontSize: sprite,         // Bind opacity to animated value
+        }}
+      >
+          123
+      </Animated.Text>
+    );
+  }
+}
+
+
+class Sprite extends React.Component {
+  render() {
     return (
       <View style={styles.container}>
-        <Image
-          source={this.getImage(this.state.frame)}
-          style={styles.sprite}
-          />
+        <Animation>
+        </Animation>
+
       </View>
     );
   }
@@ -48,6 +75,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'flex-start',
+    backgroundColor: 'blue',
     alignItems: 'stretch',
   },
   sprite: {
