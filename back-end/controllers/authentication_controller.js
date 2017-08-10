fd6a222ad7b71646d9386bcfc2fc7cc3d0c60edb
JaredTan <jared.e.tan@gmail.com>
2017-08-10 09:05:53 -0700

    touched up code

--- back-end/controllers/authentication_controller.js
+++ back-end/controllers/authentication_controller.js
@@ -23,7 +23,6 @@ exports.signup = function(req, res, next) {
     return res.status(422).json({error: "You must provide an email and password"});
   }
 
-  // Check if user already exists, send error if they do
   User.findOne({email: email}, function(err, existingUser) {
     if (err) { return next(err) }
     if (existingUser) {return res.status(422).json({error: "Email taken"})}
