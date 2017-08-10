f15f59bc64856e9ee266c2cf31ad4130721ca741
Janet Lee <lee.cjanet@gmail.com>
2017-08-09 17:37:13 -0700

    ready for git pull

---
+++ app/assets/spritesheets/background/background.js
@@ -0,0 +1,5 @@
+const BACKGROUD = {
+  image0: require('./default/tempbg.png'),
+};
+
+export default BACKGROUD;
--- app/components/sprite.js
+++ app/components/sprite.js
@@ -10,36 +10,51 @@ import {
 
 import NavBar from './navbar';
 import IMAGES from '../assets/spritesheets/sprites';
+import BACKGROUND from '../assets/spritesheets/background/background';
 
 
 class Sprite extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      frame: 0,
+      plantframe: 0,
+      bgframe: 0,
       tick: 0
     };
 
     this.getImage = this.getImage.bind(this);
   }
-  
-  getImage(num) { 
-    return IMAGES['image' + num];
+
+  getImage(arr, num) {
+    return arr['image' + num];
   }
-  
+
 
   render() {
     setTimeout(() => {
       this.setState({
-        frame: (this.state.frame + 1) % 24
+        plantframe: (this.state.plantframe + 1) % 24
       });
     }, 100);
+
+    setTimeout(() => {
+      this.setState({
+        bgframe: (this.state.bgframe + 1) % 1
+      });
+    }, 3600); //one hour
+
     return (
       <View style={styles.container}>
-        <Image 
-          source={this.getImage(this.state.frame)}  
-          style={styles.sprite}
+        <Image
+          source={this.getImage(BACKGROUND, this.state.bgframe)}
+          style={zIndex: 1, styles.background}
           />
+        <View style={styles.imagelayer}>
+          <Image
+            source={this.getImage(IMAGES, this.state.plantframe)}
+            style={styles.plant}
+            />
+        </View>
         <NavBar navigator={this.props.navigator}/>
       </View>
     );
@@ -51,12 +66,26 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'flex-start',
     alignItems: 'stretch',
+    // zIndex: 3
   },
-  sprite: {
-    flex: 1,
+  background: {
     alignSelf: 'center',
     justifyContent: 'center',
-    resizeMode: 'center'
+    resizeMode: 'cover',
+    // zIndex: 2
+  },
+  plant: {
+    alignSelf: 'center',
+    justifyContent: 'center',
+    resizeMode: 'center',
+    backgroundColor: 'red',
+    // zIndex: 1
+  },
+  imagelayer: {
+    alignSelf: 'center',
+    justifyContent: 'center',
+    resizeMode: 'cover',
+    // zIndex: 1
   }
 });
 
