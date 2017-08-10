03090fcf07d625d6d4db3c72362d2917d6aa4d9a
Keith Ryan Wong <KeithRyanWong@gmail.com>
2017-08-09 19:28:29 -0700

    Animate frames using requestAnimationFrame

--- app/components/sprite.js
+++ app/components/sprite.js
@@ -17,13 +17,24 @@ class Animation extends React.Component {
     super(props);
     this.state = {
       sprite: new Animated.Value(0),  // Initial value for opacity: 0
+      frame: 0
     };
     this.increaseSize = this.increaseSize.bind(this);
     this.resetSize = this.resetSize.bind(this);
+    this.incrementFrame = this.incrementFrame.bind(this);
+    this.getImage = this.getImage.bind(this);
   }
 
   componentDidMount() {
     this.increaseSize();                     // Starts the animation
+    this.incrementFrame();
+  }
+
+  incrementFrame() {
+    this.setState({
+      frame: (this.state.frame + 1) % 24
+    });
+    requestAnimationFrame(this.incrementFrame);
   }
 
   resetSize() {
@@ -37,23 +48,24 @@ class Animation extends React.Component {
     Animated.timing(                  // Animate over time
       this.state.sprite,            // The animated value to drive
       {
-        toValue: 100,                   // Animate to opacity: 1 (opaque)
-        duration: 1000,              // Make it take a while
+        toValue: 24,                   // Animate to opacity: 1 (opaque)
+        duration: 10000000,              // Make it take a while
       }
     ).start(this.resetSize);  
   }
 
+  getImage(frame) {
+    return IMAGES['image' + frame];
+  }
+
   render() {
     let { sprite } = this.state;
-
+    console.log(this.state.frame);
     return (
-      <Animated.Text                 // Special animatable View
-        style={{
-          fontSize: sprite,         // Bind opacity to animated value
-        }}
+      <Animated.Image                 // Special animatable View
+        source={this.getImage(this.state.frame)}
       >
-          123
-      </Animated.Text>
+      </Animated.Image>
     );
   }
 }
