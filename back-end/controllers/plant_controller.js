e8ec7c1138291677507f5dfbe345cb519240272a
Keith Ryan Wong <KeithRyanWong@gmail.com>
2017-08-11 12:59:23 -0700

    Add route to update plant

---
+++ back-end/controllers/plant_controller.js
@@ -0,0 +1,32 @@
+const Connection = require('../models/connection');
+
+exports.show = function(req, res, next) {
+  Connection.findOne({_id: req.params.connection_id}, function(err, connection) {
+    res.send({
+      plant: connection.plant
+    });
+  });
+};
+
+exports.update = function(req, res, next) {
+  const plantQuery = {_id: req.params.connection_id};
+  console.log(req);
+  const { name, lastWater, happiness, health } = req.body.plant;
+  const update = req.body.update;
+
+  console.log(update);
+
+  switch(update) {
+    case "NAME":
+      Connection.update(plantQuery, { "plant.name": name }).exec();
+      break;
+    case "WATER":
+      Connection.update(plantQuery, { "plant.lastWater": lastWater, "plant.health": health }).exec();
+      break;
+    default:
+     break;
+  }
+
+
+  res.send();
+};
