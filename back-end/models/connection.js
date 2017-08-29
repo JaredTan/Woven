4cfe2b250e77eb690431fa1a2cbdc1d8b1135a19
Keith Ryan Wong <KeithRyanWong@gmail.com>
2017-08-28 19:25:08 -0700

    Add messages to plant backend

--- back-end/models/connection.js
+++ back-end/models/connection.js
@@ -1,19 +1,23 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+
 var connectionSchema = new Schema({
   todos: [
     {
-      text: {type: String}
+      text: { type: String }
     }
   ],
   plant: {
-    name: {type: String, default: 'Greggles'},
-    health: {type: Number, default: 10},
-    happiness: {type: Number, default: 75},
-    age: {type: Number, default: 0},
+    name: { type: String, default: 'Greggles' },
+    health: { type: Number, default: 10 },
+    happiness: { type: Number, default: 75 },
+    age: { type: Number, default: 0},
     seeded: { type: Date, default: Date.now() },
-    lastWater: { type: Date, default: Date.now() }
+    lastWater: { type: Date, default: Date.now() },
+    messages: {
+      for: { type: Object }
+    }
   }
 });
 
